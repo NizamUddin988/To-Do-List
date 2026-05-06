8a3d2ba79bf0032dbb64a89b5a6e47e55a59e693
Nizam Uddin <nizampasha218@gmail.com>
2026-05-06 14:21:29 +0530

    Update default task text in App component

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const addTask = () => {
     const newTask = {
       id: Math.random(),
-      text: "Enter Task",
+      text: "Enter your task here..",
       time: times,
       checked: false
     };
